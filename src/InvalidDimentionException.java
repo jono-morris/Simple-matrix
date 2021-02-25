232b0b51259c15aa3b71997351fb333b91c4288d
Jono <jono.morris@xtra.co.nz>
2021-02-25 21:38:20 +1300

    Remove moved classes.

--- src/InvalidDimentionException.java
+++
@@ -1,9 +0,0 @@
-
-public class InvalidDimentionException extends Exception {
-
-    private static final long serialVersionUID = 1L;
-
-    public InvalidDimentionException(String message) {
-        super(message);
-    }
-}
